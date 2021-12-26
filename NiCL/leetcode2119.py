8ce550c82459d55716a5863cd77cbae70672c2e5
NiChloride <cl980820@hotmail.com>
2021-12-26 12:15:19 +0800

    weekly challenge 273

---
+++ NiCL/leetcode2119.py
@@ -0,0 +1,16 @@
+# A Number After a Double Reversal
+
+class Solution:
+    #helper function
+    def reverseInteger (self, num: int) -> int:
+        if num<10:
+            return num
+        str_num = str(num)
+        str_num = str_num[::-1]
+        reversed_num = int(str_num)
+        return reversed_num
+        
+    def isSameAfterReversals(self, num: int) -> bool:
+        double_reversed = self.reverseInteger(self.reverseInteger(num))
+        return double_reversed == num
+        
